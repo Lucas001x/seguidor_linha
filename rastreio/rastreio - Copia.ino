231177bc8fcc67c20d918999864f9a87bed782fc
Lucas001x <lucascezararruda@gmail.com>
2024-05-22 00:14:54 -0300

    atulizado 1

--- rastreio/rastreio - Copia.ino
+++ rastreio/rastreio - Copia.ino
@@ -1,5 +1,7 @@
 #include <Ultrasonic.h>// Inicia a biblioteca do sensor ultrassônico
 
+//teste de atuali. git
+
 //Define os pinos trigger, echo e o pino do botão
 #define pino_trigger 27
 #define pino_echo 14
